595444a9c1fc22d6b0a281fc6ae748b9f37a868b
Yunus Emre Şen <90857438+voselef@users.noreply.github.com>
2025-10-02 22:45:49 +0300

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    int s1, s2, s3;
-    printf("Uc sayi giriniz:\n");
-    scanf("%d %d %d", &s1, &s2, &s3);
+    while(1){
+        int s1, s2, s3;
+        printf("Uc sayi giriniz:\n");
+        scanf("%d %d %d", &s1, &s2, &s3);
 
-    int gecici;
-    if(s1 > s2){ gecici = s1; s1 = s2; s2 = gecici; }
-    if(s1 > s3){ gecici = s1; s1 = s3; s3 = gecici; }
-    if(s2 > s3){ gecici = s2; s2 = s3; s3 = gecici; }
+        int gecici;
+        if(s1 > s2){ gecici = s1; s1 = s2; s2 = gecici; }
+        if(s1 > s3){ gecici = s1; s1 = s3; s3 = gecici; }
+        if(s2 > s3){ gecici = s2; s2 = s3; s3 = gecici; }
 
-    printf("Sirali: %d %d %d\n", s1, s2, s3);
+        printf("Sirali: %d %d %d\n", s1, s2, s3);
+        printf("\n\nIslem bitti. Basa donuluyor...\n\n");
+    }
 }
